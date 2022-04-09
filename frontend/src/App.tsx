8abd39d8e472162f8e7bc5a5165eb8d767e67319
lajarre <alexandre.hajjar@gmail.com>
2022-04-09 21:50:59 +0100

    v1 pie chart with balances

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
-import React, { useCallback } from 'react'
+import { ReactElement, useState, useEffect } from 'react'
 import styled from 'styled-components'
-import { Button, Title } from '@gnosis.pm/safe-react-components'
+import { Title } from '@gnosis.pm/safe-react-components'
+
+import { PieChart } from 'react-minimal-pie-chart'
+
 import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk'
+import SafeAppsSDK from '@gnosis.pm/safe-apps-sdk/dist/src/sdk'
+
+type AsyncReturnType<T extends (...args: any) => Promise<any>> = T extends (...args: any) => Promise<infer R> ? R : any
 
 const Container = styled.div`
   padding: 1rem;
@@ -13,43 +19,89 @@ const Container = styled.div`
   flex-direction: column;
 `
 
-const Link = styled.a`
-  margin-top: 8px;
+// const Link = styled.a`
+//   margin-top: 8px;
+// `
+
+type TagProps = {
+  background: string
+}
+const Tag = styled.span<TagProps>`
+  padding: 0.3em;
+  border-radius: 1em;
+  position: relative;
+  background: ${(props) => props.background}40;
+  text-align: center;
+  line-height: 50px;
 `
 
-const SafeApp = (): React.ReactElement => {
-  const { sdk, safe } = useSafeAppsSDK()
+const getBalances = async (sdk: SafeAppsSDK) => {
+  const safeBalanceResponse = await sdk.safe.experimental_getBalances()
+  const ERC20Balances = safeBalanceResponse.items.filter(
+    (token) => token.tokenInfo.type === 'ERC20' || token.tokenInfo.type === 'NATIVE_TOKEN',
+  )
+  const simpleBalances = ERC20Balances.map((token) => ({
+    name: token.tokenInfo.name,
+    symbol: token.tokenInfo.symbol,
+    balance: parseFloat(token.balance),
+    // ⚠️ Hardcoded GNT:USD
+    fiatBalance:
+      parseFloat(token.fiatConversion) === 0
+        ? parseFloat(token.balance) / 1000000000000000000000000
+        : parseFloat(token.fiatBalance),
+  }))
+  console.debug(simpleBalances)
+  return simpleBalances
+}
 
-  const submitTx = useCallback(async () => {
-    try {
-      const { safeTxHash } = await sdk.txs.send({
-        txs: [
-          {
-            to: safe.safeAddress,
-            value: '0',
-            data: '0x',
-          },
-        ],
-      })
-      console.log({ safeTxHash })
-      const safeTx = await sdk.txs.getBySafeTxHash(safeTxHash)
-      console.log({ safeTx })
-    } catch (e) {
-      console.error(e)
-    }
-  }, [safe, sdk])
+const colorOfToken = (symbol: string) => {
+  switch (symbol) {
+    case 'DAI':
+      return 'yellow'
+    case 'GNT':
+      return '#DDDDE3'
+    case 'ETH':
+      return '#000AFF'
+    default:
+      return '#EF0000'
+  }
+}
+
+const SafeApp = (): ReactElement => {
+  const { sdk } = useSafeAppsSDK()
+  const [balances, setBalances] = useState<AsyncReturnType<typeof getBalances>>([])
+
+  useEffect(() => {
+    ;(async () => {
+      setBalances(await getBalances(sdk))
+    })()
+  }, [sdk])
+
+  const pieChartData = balances.map((token) => ({
+    title: token.symbol,
+    value: token.fiatBalance,
+    color: colorOfToken(token.symbol),
+  }))
+
+  const totalFiat = balances.map((token) => token.fiatBalance).reduce((a, b) => a + b, 0)
 
   return (
     <Container>
-      <Title size="md">Safe: {safe.safeAddress}</Title>
-
-      <Button size="lg" color="primary" onClick={submitTx}>
-        Click to send a test transaction
-      </Button>
-
-      <Link href="https://github.com/gnosis/safe-apps-sdk" target="_blank" rel="noreferrer">
-        Documentation
-      </Link>
+      <Title size="md">Treasury Portfolio</Title>
+      <PieChart data={pieChartData} paddingAngle={0.5} lineWidth={25} startAngle={-90} radius={20} animate={true} />
+      <table>
+        <tbody>
+          {balances.map((token) => (
+            <tr>
+              <td>
+                <Tag background={colorOfToken(token.symbol)}>$ {token.symbol}</Tag>
+              </td>
+              <td>{token.name}</td>
+              <td>{Math.round((100 * token.fiatBalance) / totalFiat)}%</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Container>
   )
 }
