d17c809d7b419eba6d3968b310332612f2ecc550
lajarre <alexandre.hajjar@gmail.com>
2022-04-10 17:03:22 +0100

    visual improvements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { ReactElement, useState, useEffect, useMemo } from 'react'
 import styled from 'styled-components'
 import { PieChart } from 'react-minimal-pie-chart'
 import { ethers } from 'ethers'
-import { Button, Title } from '@gnosis.pm/safe-react-components'
+import { Button, Title, Card, Divider, GenericModal, Select } from '@gnosis.pm/safe-react-components'
 import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk'
 import SafeAppsSDK from '@gnosis.pm/safe-apps-sdk/dist/src/sdk'
 import { SafeAppProvider } from '@gnosis.pm/safe-apps-provider'
@@ -159,12 +159,79 @@ const DeployPoolButton = (): ReactElement => {
       }}
       size="lg"
     >
-      deploy pool
+      Deploy
     </Button>
   )
 }
 
 /////////////
+
+const ModalButton = (): ReactElement => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [weightsItemId, setWeightsItemId] = useState('0')
+
+  const [activeCommitmentItemId, setActiveCommitmentItemId] = useState('0')
+  const [durationItemId, setDurationItemId] = useState('2')
+
+  return (
+    <>
+      <Button size="md" color="primary" onClick={() => setIsOpen(!isOpen)}>
+        View
+      </Button>
+      {isOpen && (
+        <GenericModal
+          onClose={() => setIsOpen(false)}
+          title="Strategy setup"
+          body={
+            <div>
+              <Select
+                name="weights"
+                label="Weights"
+                activeItemId={weightsItemId}
+                onItemClick={setWeightsItemId}
+                items={[
+                  { id: '0', label: '20% GNT / 80% DAI' },
+                  { id: '1', label: '50% GNT / 50% DAI' },
+                  { id: '2', label: '80% GNT / 20% DAI' },
+                ]}
+              />
+              <p></p>
+              <Select
+                name="commitment"
+                label="Treasury committed"
+                activeItemId={activeCommitmentItemId}
+                onItemClick={setActiveCommitmentItemId}
+                items={[
+                  { id: '0', label: '10%' },
+                  { id: '1', label: '20%' },
+                  { id: '2', label: '50%' },
+                  { id: '3', label: '80%' },
+                  { id: '4', label: '100%' },
+                ]}
+              />
+              <p></p>
+
+              <Select
+                name="duration"
+                label="Duration"
+                activeItemId={durationItemId}
+                onItemClick={setDurationItemId}
+                items={[
+                  { id: '0', label: '1 month' },
+                  { id: '1', label: '3 months' },
+                  { id: '2', label: '6 months' },
+                  { id: '3', label: '1 year' },
+                  { id: '4', label: '2 years' },
+                ]}
+              />
+            </div>
+          }
+          footer={<DeployPoolButton />}
+        />
+      )}
+    </>
+  )
+}
 
 const SafeApp = (): ReactElement => {
   const { sdk } = useSafeAppsSDK()
@@ -186,22 +253,54 @@ const SafeApp = (): ReactElement => {
 
   return (
     <Container>
-      <Title size="md">Treasury Portfolio</Title>
-      <PieChart data={pieChartData} paddingAngle={0.5} lineWidth={25} startAngle={-90} radius={20} animate={true} />
+      <img src="./applogo.png" alt="Logo" style={{ width: '30%', marginBottom: '50px' }} />
       <Table>
         <tbody>
-          {balances.map((token) => (
-            <tr key={token.symbol}>
-              <td>
-                <Tag background={colorOfToken(token.symbol)}>$ {token.symbol}</Tag>
-              </td>
-              <td>{token.name}</td>
-              <td>{Math.round((100 * token.fiatBalance) / totalFiat)}%</td>
-            </tr>
-          ))}
+          <tr>
+            <td>
+              <Title size="md">Treasury Portfolio</Title>
+
+              <PieChart
+                data={pieChartData}
+                paddingAngle={0.5}
+                lineWidth={25}
+                startAngle={-90}
+                radius={20}
+                animate={true}
+              />
+              <Table>
+                <tbody>
+                  {balances.map((token) => (
+                    <tr key={token.symbol}>
+                      <td>
+                        <Tag background={colorOfToken(token.symbol)}>$ {token.symbol}</Tag>
+                      </td>
+                      <td>{token.name}</td>
+                      <td>{Math.round((100 * token.fiatBalance) / totalFiat)}%</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </td>
+            <td>
+              <Card style={{ height: '100%', textAlign: 'center' }}>
+                <Title size="sm">Current templates</Title>
+                <Title size="xs">Stables</Title>
+                <p>Pair DAO governance tokens with DAI</p>
+                <ModalButton />
+                <Divider />
+                <Title size="xs">ETH</Title>
+                <p>Pair DAO governance tokens with ETH</p>
+                <ModalButton />
+                <Divider />
+                <Title size="xs">Yield</Title>
+                <p>Pair DAO governance tokens with Lido staked ETH</p>
+                <ModalButton />
+              </Card>
+            </td>
+          </tr>
         </tbody>
       </Table>
-      <DeployPoolButton />
     </Container>
   )
 }
