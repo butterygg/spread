dd137cd50d75036b9b849198baa42bd50c2ddf76
Kevin Huo <kevin.w.huo@gmail.com>
2022-04-10 10:37:48 -0400

    modify deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,7 +20,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: {
     compilers: [
-      { version: "0.8.4" },
+      { version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
       { version: "0.7.1",
         settings: {} 
       },
--- scripts/deploy-stream-swap-one-way.js
+++ scripts/deploy-stream-swap-one-way.js
@@ -6,9 +6,15 @@ async function main() {
     const weiAmount = (await deployer.getBalance()).toString();
     
     console.log("Account balance:", (await ethers.utils.formatEther(weiAmount)));
-  
+
+    // TODO: replace owner here with gnosis safe
+    const owner = '0x0C804A7D83E8A883aF632E0090272b9E837aB78a';
+    const host = '0xeD5B5b32110c3Ded02a07c8b8e97513FAfb883B6';
+    const cfa = '0xF4C5310E51F6079F601a5fb7120bC72a70b96e2A';
+    const ida = '0x32E0ecb72C1dDD92B007405F8102c1556624264D';
+    const registrationKey = '';
     const StreamSwapOneWayMarket = await ethers.getContractFactory("StreamSwapOneWayMarket");
-    const streamSwapOneWayMarket = await StreamSwapOneWayMarket.deploy();
+    const streamSwapOneWayMarket = await StreamSwapOneWayMarket.deploy(owner, host, cfa, ida, registrationKey);
   
     console.log("StreamSwapOneWayMarket address:", streamSwapOneWayMarket.address);
   }
