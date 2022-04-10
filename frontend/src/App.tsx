1cbb7acc469e306a7f13a5bb48b182b2c2e2feec
lajarre <alexandre.hajjar@gmail.com>
2022-04-10 10:13:05 +0100

    WIP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import { ReactElement, useState, useEffect } from 'react'
+import { ReactElement, useState, useEffect, useMemo } from 'react'
 import styled from 'styled-components'
-import { Title } from '@gnosis.pm/safe-react-components'
-
 import { PieChart } from 'react-minimal-pie-chart'
-
+import { ethers } from 'ethers'
+import { Button, Title } from '@gnosis.pm/safe-react-components'
 import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk'
 import SafeAppsSDK from '@gnosis.pm/safe-apps-sdk/dist/src/sdk'
+import { SafeAppProvider } from '@gnosis.pm/safe-apps-provider'
+
+// import WEIGHTED_POOL_FACTORY_ABI from './weightedpoolfactory.json'
+import WEIGHTED_POOL_ABI from './weightedpool.json'
+import VAULT_ABI from './balancervault.json'
+// import ERC20_ABI from './erc20.json'
 
 type AsyncReturnType<T extends (...args: any) => Promise<any>> = T extends (...args: any) => Promise<infer R> ? R : any
 
@@ -18,10 +23,6 @@ const Container = styled.div`
   align-items: center;
   flex-direction: column;
 `
-
-// const Link = styled.a`
-//   margin-top: 8px;
-// `
 
 const Table = styled.table`
   text-align: left;
@@ -44,6 +45,7 @@ const getBalances = async (sdk: SafeAppsSDK) => {
   const ERC20Balances = safeBalanceResponse.items.filter(
     (token) => token.tokenInfo.type === 'ERC20' || token.tokenInfo.type === 'NATIVE_TOKEN',
   )
+  console.log({ ERC20Balances })
   const simpleBalances = ERC20Balances.map((token) => ({
     name: token.tokenInfo.name,
     symbol: token.tokenInfo.symbol,
@@ -51,17 +53,17 @@ const getBalances = async (sdk: SafeAppsSDK) => {
     // ⚠️ Hardcoded GNT:USD
     fiatBalance:
       parseFloat(token.fiatConversion) === 0
-        ? parseFloat(token.balance) / 1000000000000000000000000
+        ? parseFloat(token.balance) / 10000000000000000000000
         : parseFloat(token.fiatBalance),
   }))
-  console.debug(simpleBalances)
+  console.debug({ simpleBalances })
   return simpleBalances
 }
 
 const colorOfToken = (symbol: string) => {
   switch (symbol) {
     case 'DAI':
-      return 'yellow'
+      return '#fff000'
     case 'GNT':
       return '#DDDDE3'
     case 'ETH':
@@ -70,6 +72,99 @@ const colorOfToken = (symbol: string) => {
       return '#EF0000'
   }
 }
+
+/////////////
+
+const deployPool = async (provider: ethers.providers.Web3Provider, safeAddr: string) => {
+  // Contracts
+  const VAULT = '0xBA12222222228d8Ba445958a75a0704d566BF2C8'
+  // const WEIGHTED_POOL_FACTORY = '0x8E9aa87E45e92bad84D5F8DD1bff34Fb92637dE9'
+  // const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+  // Tokens -- MUST be sorted numerically
+  const GNT = '0x33c41eE5647c012f8CE9930EE05FC7aF86244921'
+  const DAI = '0x50c075b3Dc738D3E6372975F74101B0e5780f58f'
+  const tokens = [GNT, DAI]
+
+  // const NAME = 'GovernanceToken DAI pool'
+  // const SYMBOL = '10GNT-90DAI'
+  // const swapFeePercentage = BigInt(0.005e18) // 0.5%
+  // const weights = [BigInt(0.1e18), BigInt(0.9e18)]
+
+  // const factory = new ethers.Contract(WEIGHTED_POOL_FACTORY, WEIGHTED_POOL_FACTORY_ABI, provider.getSigner())
+
+  // const tx = await factory.create(NAME, SYMBOL, tokens, weights, swapFeePercentage, ZERO_ADDRESS)
+  // const receipt = await tx.wait()
+
+  // // We need to get the new pool address out of the PoolCreated event
+  // const events = receipt.events.filter((e: any) => e.event === 'PoolCreated')
+  // const poolAddress = events[0].args.pool
+  // console.log({ poolAddress })
+
+  const poolAddress = '0xde620bb8be43ee54d7aa73f8e99a7409fe511084'
+
+  // We're going to need the PoolId later, so ask the contract for it
+  // const pool = await ethers.getContractAt('WeightedPool', poolAddress)
+  const pool = new ethers.Contract(poolAddress, WEIGHTED_POOL_ABI, provider)
+  const poolId = await pool.getPoolId()
+
+  console.log({ poolId })
+
+  const vault = new ethers.Contract(VAULT, VAULT_ABI, provider.getSigner())
+  console.log({ vault })
+
+  // Tokens must be in the same order
+  // Values must be decimal-normalized!
+  const initialBalances = [BigInt(1e6), BigInt(1e6)]
+  console.log({ initialBalances })
+
+  // // Need to approve the Vault to transfer the tokens!
+  // // Can do through Etherscan, or programmatically
+  // for (const i in tokens) {
+  //   const tokenContract = new ethers.Contract(tokens[i], ERC20_ABI, provider.getSigner())
+  //   await tokenContract.approve(VAULT, initialBalances[i])
+  // }
+
+  // Construct userData
+  const JOIN_KIND_INIT = 0
+  const initUserData = ethers.utils.defaultAbiCoder.encode(['uint256', 'uint256[]'], [JOIN_KIND_INIT, initialBalances])
+
+  const joinPoolRequest = {
+    assets: tokens,
+    maxAmountsIn: initialBalances,
+    userData: initUserData,
+    fromInternalBalance: false,
+  }
+
+  // joins are done on the Vault
+  const joinTx = await vault.joinPool(poolId, safeAddr, safeAddr, joinPoolRequest)
+  console.log({ joinTx })
+
+  // You can wait for it like this, or just print the tx hash and monitor
+  const receipt = await joinTx.wait()
+  console.log({ receipt })
+}
+
+const DeployPoolButton = (): ReactElement => {
+  const { sdk, safe } = useSafeAppsSDK()
+  const web3Provider = useMemo(() => new ethers.providers.Web3Provider(new SafeAppProvider(safe, sdk)), [sdk, safe])
+
+  return (
+    <Button
+      onClick={() => {
+        ;(async () => {
+          const { safeAddress } = await sdk.safe.getInfo()
+          await deployPool(web3Provider, safeAddress)
+        })()
+      }}
+      size="lg"
+    >
+      deploy pool
+    </Button>
+  )
+}
+
+/////////////
 
 const SafeApp = (): ReactElement => {
   const { sdk } = useSafeAppsSDK()
@@ -96,7 +191,7 @@ const SafeApp = (): ReactElement => {
       <Table>
         <tbody>
           {balances.map((token) => (
-            <tr>
+            <tr key={token.symbol}>
               <td>
                 <Tag background={colorOfToken(token.symbol)}>$ {token.symbol}</Tag>
               </td>
@@ -106,6 +201,7 @@ const SafeApp = (): ReactElement => {
           ))}
         </tbody>
       </Table>
+      <DeployPoolButton />
     </Container>
   )
 }
